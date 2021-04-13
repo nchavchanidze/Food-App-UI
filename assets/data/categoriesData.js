e6b50096365ed6ecf8a1335ceb67e90bdc19c8ee
Nikoloz Tchavtchanidze <45031920+nchavchanidze@users.noreply.github.com>
2021-04-13 11:08:49 +0400

    Update Categories Icons

--- assets/data/categoriesData.js
+++ assets/data/categoriesData.js
@@ -1,41 +1,52 @@
 const categoriesData = [
-    {
-      id: '1',
-      image: require('../images/pizza-icon.png'),
-      title: 'Pizza',
-      selected: true,
-    },
-    {
-      id: '2',
-      image: require('../images/shrimp-icon.png'),
-      title: 'Seafood',
-      selected: false,
-    },
-    {
-      id: '3',
-      image: require('../images/soda-icon.png'),
-      title: 'Soft Drinks',
-      selected: false,
-    },
-    {
-      id: '4',
-      image: require('../images/pizza-icon.png'),
-      title: 'Pizza',
-      selected: false,
-    },
-    {
-      id: '5',
-      image: require('../images/shrimp-icon.png'),
-      title: 'Seafood',
-      selected: false,
-    },
-    {
-      id: '6',
-      image: require('../images/soda-icon.png'),
-      title: 'Soft Drinks',
-      selected: false,
-    },
-  ];
-  
-  export default categoriesData;
-  
+  {
+    id: '1',
+    image: require('../images/pizza.png'),
+    title: 'Pizza',
+    selected: true,
+  },
+  {
+    id: '2',
+    image: require('../images/shrimp-icon.png'),
+    title: 'Seafood',
+    selected: false,
+  },
+  {
+    id: '3',
+    image: require('../images/soda-icon.png'),
+    title: 'Soft Drinks',
+    selected: false,
+  },
+  {
+    id: '4',
+    image: require('../images/tuna.png'),
+    title: 'Asian',
+    selected: false,
+  },
+  {
+    id: '5',
+    image: require('../images/soda.png'),
+    title: 'Soda Drinks',
+    selected: false,
+  },
+  {
+    id: '6',
+    image: require('../images/hamburger.png'),
+    title: 'Burgers',
+    selected: false,
+  },
+  {
+    id: '7',
+    image: require('../images/steak.png'),
+    title: 'Steak',
+    selected: false,
+  },
+  {
+    id: '8',
+    image: require('../images/ice-cream.png'),
+    title: 'Deserts',
+    selected: false,
+  },
+];
+
+export default categoriesData;
